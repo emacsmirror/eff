e4ed202c2347f389b93b68dfdda105b622168e15
Michael Krasnyk <michael.krasnyk@gmail.com>
2023-06-13 11:43:40 +0200

    rename repository

--- eff.el
+++ eff.el
@@ -1,9 +1,9 @@
-;;; eff-mode.el --- Show symbols in Executable File Formats -*- lexical-binding: t; mode: emacs-lisp -*-
+;;; eff.el --- Show symbols in Executable File Formats -*- lexical-binding: t; mode: emacs-lisp -*-
 
 ;; Copyright (C) 2020-2023 Michael Krasnyk
 
 ;; Michael Krasnyk <michael.krasnyk@gmail.com>
-;; URL: https://github.com/oxidase/eff-mode
+;; URL: https://github.com/oxidase/eff
 ;; Package-Requires: ((emacs "28"))
 ;; Version: 1.0
 ;; Keywords: ELF readelf convenience
@@ -39,30 +39,30 @@
 ;; Customizable variables
 (defgroup eff nil "EFF mode customizable variables."
           :group 'tools
-          :prefix "eff-mode-"
-          :link '(url-link "https://github.com/oxidase/eff-mode"))
+          :prefix "eff-"
+          :link '(url-link "https://github.com/oxidase/eff"))
 
-(defcustom eff-mode-md5sum "md5sum"
+(defcustom eff-md5sum "md5sum"
   "MD5 sum executable name or path."
   :type 'string
   :group 'eff)
 
-(defcustom eff-mode-readelf "readelf"
+(defcustom eff-readelf "readelf"
    "The readelf executable name or path."
   :type 'string
   :group 'eff)
 
-(defcustom eff-mode-strings "strings"
+(defcustom eff-strings "strings"
   "The strings executable name or path."
   :type 'string
   :group 'eff)
 
-(defcustom eff-mode-buffer-initial-type 'dynamic
+(defcustom eff-buffer-initial-type 'dynamic
   "The initial state of an ELF buffer."
   :type 'symbol
   :group 'eff)
 
-(defcustom eff-mode-use-local-toolchain t
+(defcustom eff-use-local-toolchain t
   "Use a toolchain on local or remote machine.
 
   TODO: Using toolchains remotely is not implemented, required commit
@@ -70,7 +70,7 @@
   :type 'boolean
   :group 'eff)
 
-(defcustom eff-mode-gdb-alist `(("00b7" . "aarch64-linux-gnu-gdb")
+(defcustom eff-gdb-alist `(("00b7" . "aarch64-linux-gnu-gdb")
                                 ("0028" . "arm-none-eabi-gdb"))
   "The gdb binary used by used by the corresponding platform.
 
@@ -83,56 +83,56 @@ Each element has the form (E_MACHINE . GDB).
 
 
 ;; Faces
-(defface eff-mode-disassemble-hex
+(defface eff-disassemble-hex
   '((((class color) (background light)) :foreground "grey30")
     (((class color) (background  dark)) :foreground "grey80"))
   "Face for the disassembled hex values.")
 
-(defface eff-mode-disassemble-opcode
+(defface eff-disassemble-opcode
   '((((class color) (background light)) :foreground "blue"))
   "Face for the disassembled opcode values.")
 
 
 ;; Local variables
-(defvar-local eff-mode-buffer-type eff-mode-buffer-initial-type)
+(defvar-local eff-buffer-type eff-buffer-initial-type)
 
-(defvar-local eff-mode-gdb-executable "gdb")
+(defvar-local eff-gdb-executable "gdb")
 
-(defvar-local eff-mode-disassemble-command
+(defvar-local eff-disassemble-command
   "%s -n -q -batch -ex 'file %s' -ex 'disassemble/rs %s'")
 
-(defvar-local eff-mode-binary-command
+(defvar-local eff-binary-command
   "dd status=none bs=1 skip=%s count=%s if=%s")
 
 (defvar-local eff-asm-comment-char ?#)
 
 
 ;; Constants
-(defconst eff-mode-buffer-types
-  `((arch-specific   . ((key . "A") (command . ,(append (split-string eff-mode-readelf) '("-W" "--arch-specific")))))
-    (archive-index   . ((key . "c") (command . ,(append (split-string eff-mode-readelf) '("-W" "--archive-index")))))
-    (dynamic         . ((key . "d") (command . ,(append (split-string eff-mode-readelf) '("-W" "--dynamic")))))
-    (headers         . ((key . "e") (command . ,(append (split-string eff-mode-readelf) '("-W" "--headers")))))
-    (section-groups  . ((key . "G") (command . ,(append (split-string eff-mode-readelf) '("-W" "--section-groups")))))
-    (header          . ((key . "h") (command . ,(append (split-string eff-mode-readelf) '("-W" "--file-header")))))
-    (histogram       . ((key . "I") (command . ,(append (split-string eff-mode-readelf) '("-W" "--histogram")))))
-    (program-headers . ((key . "l") (command . ,(append (split-string eff-mode-readelf) '("-W" "--program-headers")))))
-    (md5sum          . ((key . "m") (command . ,(split-string eff-mode-md5sum))))
-    (notes           . ((key . "n") (command . ,(append (split-string eff-mode-readelf) '("-W" "--notes")))))
-    (relocs          . ((key . "r") (command . ,(append (split-string eff-mode-readelf) '("-W" "--relocs")))))
-    (section-headers . ((key . "S") (command . ,(append (split-string eff-mode-readelf) '("-W" "--section-headers")))))
-    (symbols         . ((key . "s") (command . ,(append (split-string eff-mode-readelf) '("-W" "--symbols")))))
-    (unwind          . ((key . "u") (command . ,(append (split-string eff-mode-readelf) '("-W" "--unwind")))))
-    (version-info    . ((key . "V") (command . ,(append (split-string eff-mode-readelf) '("-W" "--version-info")))))
-    (dyn-syms        . ((key . "x") (command . ,(append (split-string eff-mode-readelf) '("-W" "--dyn-syms")))))
-    (strings         . ((key . "z") (command . ,(split-string eff-mode-strings))))))
+(defconst eff-buffer-types
+  `((arch-specific   . ((key . "A") (command . ,(append (split-string eff-readelf) '("-W" "--arch-specific")))))
+    (archive-index   . ((key . "c") (command . ,(append (split-string eff-readelf) '("-W" "--archive-index")))))
+    (dynamic         . ((key . "d") (command . ,(append (split-string eff-readelf) '("-W" "--dynamic")))))
+    (headers         . ((key . "e") (command . ,(append (split-string eff-readelf) '("-W" "--headers")))))
+    (section-groups  . ((key . "G") (command . ,(append (split-string eff-readelf) '("-W" "--section-groups")))))
+    (header          . ((key . "h") (command . ,(append (split-string eff-readelf) '("-W" "--file-header")))))
+    (histogram       . ((key . "I") (command . ,(append (split-string eff-readelf) '("-W" "--histogram")))))
+    (program-headers . ((key . "l") (command . ,(append (split-string eff-readelf) '("-W" "--program-headers")))))
+    (md5sum          . ((key . "m") (command . ,(split-string eff-md5sum))))
+    (notes           . ((key . "n") (command . ,(append (split-string eff-readelf) '("-W" "--notes")))))
+    (relocs          . ((key . "r") (command . ,(append (split-string eff-readelf) '("-W" "--relocs")))))
+    (section-headers . ((key . "S") (command . ,(append (split-string eff-readelf) '("-W" "--section-headers")))))
+    (symbols         . ((key . "s") (command . ,(append (split-string eff-readelf) '("-W" "--symbols")))))
+    (unwind          . ((key . "u") (command . ,(append (split-string eff-readelf) '("-W" "--unwind")))))
+    (version-info    . ((key . "V") (command . ,(append (split-string eff-readelf) '("-W" "--version-info")))))
+    (dyn-syms        . ((key . "x") (command . ,(append (split-string eff-readelf) '("-W" "--dyn-syms")))))
+    (strings         . ((key . "z") (command . ,(split-string eff-strings))))))
 
 
 ;;
-(defun eff-mode-buffer-file-name ()
+(defun eff-buffer-file-name ()
   "Get local file name of a buffer."
   (cond
-   ((and eff-mode-use-local-toolchain (fboundp 'tramp-tramp-file-p) (tramp-tramp-file-p (buffer-file-name)))
+   ((and eff-use-local-toolchain (fboundp 'tramp-tramp-file-p) (tramp-tramp-file-p (buffer-file-name)))
     (tramp-handle-file-local-copy (buffer-file-name)))
    (t (buffer-file-name))))
 
@@ -144,8 +144,8 @@ Each element has the form (E_MACHINE . GDB).
            (ol (make-button
                 (match-beginning 2) (match-end 2)
                 'help-echo (format "disassemble %s" name)
-                'action #'eff-mode-disassemble
-                'mouse-action #'eff-mode-disassemble)))
+                'action #'eff-disassemble
+                'mouse-action #'eff-disassemble)))
       (overlay-put ol 'symbol name))))
 
 (defun eff-elf-add-sections-refs ()
@@ -159,8 +159,8 @@ Each element has the form (E_MACHINE . GDB).
              (ol (make-button
                   (match-beginning 1) (match-end 1)
                   'help-echo (format "hexl %s" (match-string 1))
-                  'action #'eff-mode-binary
-                  'mouse-action #'eff-mode-binary)))
+                  'action #'eff-binary
+                  'mouse-action #'eff-binary)))
         (overlay-put ol 'section name)
         (overlay-put ol 'offset offset)
         (overlay-put ol 'size size)))))
@@ -169,19 +169,19 @@ Each element has the form (E_MACHINE . GDB).
   "Revert buffer with ELF-specific modes."
   (interactive)
   (when (eq 'eff-mode major-mode)
-    (let* ((state (cdr (assoc eff-mode-buffer-type eff-mode-buffer-types)))
+    (let* ((state (cdr (assoc eff-buffer-type eff-buffer-types)))
            (command (cdr (assoc 'command state)))
            (stdout (current-buffer))
            (inhibit-read-only t)
            (stderr (generate-new-buffer "*readelf stderr*"))
-           (file-name (eff-mode-buffer-file-name))
+           (file-name (eff-buffer-file-name))
            (default-directory (file-name-directory file-name)))
       (setf (buffer-string) "")
       (let* ((get_e_machine_command
               (format "hexdump -e '1/2 \"%s\"' -s 0x12 -n 2 %s" "%04x" (file-name-nondirectory file-name)))
              (e_machine (string-trim (shell-command-to-string get_e_machine_command)))
-             (gdb (assoc e_machine eff-mode-gdb-alist)))
-        (if gdb (setq-local eff-mode-gdb-executable (cdr gdb))))
+             (gdb (assoc e_machine eff-gdb-alist)))
+        (if gdb (setq-local eff-gdb-executable (cdr gdb))))
 
       (make-process ;; TODO: switch to tramp-handle-make-process
        :name "readelf"
@@ -202,11 +202,11 @@ Each element has the form (E_MACHINE . GDB).
            (when (string= event "finished\n")
              (cond
                ((and (not (string= "a" (file-name-extension file-name))) ;; TODO: add offsets for *.o files shown by "ar tO"
-                 (or (eq eff-mode-buffer-type 'headers)
-                     (eq eff-mode-buffer-type 'section-headers)))
+                 (or (eq eff-buffer-type 'headers)
+                     (eq eff-buffer-type 'section-headers)))
                 (eff-elf-add-sections-refs))
-               ((or (eq eff-mode-buffer-type 'dyn-syms)
-                    (eq eff-mode-buffer-type 'symbols))
+               ((or (eq eff-buffer-type 'dyn-syms)
+                    (eq eff-buffer-type 'symbols))
                 (eff-elf-add-func-refs))))
            (goto-char (point-min))
            (set-buffer-modified-p nil)
@@ -217,13 +217,13 @@ Each element has the form (E_MACHINE . GDB).
                (message "eff-mode: %s\n%s" event err))))
          (kill-buffer stderr))))))
 
-(defun eff-mode-disassemble (overlay)
+(defun eff-disassemble (overlay)
   "Mode for disassembled code for a symbols OVERLAY."
   (let* ((symbol (overlay-get overlay 'symbol))
          (buffer-name (format "%s(%s)" (buffer-name) symbol))
-         (file-name (eff-mode-buffer-file-name))
+         (file-name (eff-buffer-file-name))
          (default-directory (file-name-directory file-name))
-         (command (format eff-mode-disassemble-command eff-mode-gdb-executable (file-name-nondirectory file-name) symbol)))
+         (command (format eff-disassemble-command eff-gdb-executable (file-name-nondirectory file-name) symbol)))
     (with-current-buffer (pop-to-buffer buffer-name)
       (shell-command command (current-buffer))
       (flush-lines "^[[:space:]]*$" (point-min) (point-max))
@@ -238,20 +238,20 @@ Each element has the form (E_MACHINE . GDB).
                (beg3 (+ beg2 (length hex)))
                (end3 (+ beg3 (length opcode))))
           (replace-match (concat offset hex opcode))
-          (put-text-property beg2 beg3 'font-lock-face 'eff-mode-disassemble-hex)
-          (put-text-property beg3 end3 'font-lock-face 'eff-mode-disassemble-opcode)))
+          (put-text-property beg2 beg3 'font-lock-face 'eff-disassemble-hex)
+          (put-text-property beg3 end3 'font-lock-face 'eff-disassemble-opcode)))
       (setq-local eff-asm-comment-char ?#)
       (read-only-mode))))
 
-(defun eff-mode-binary (overlay)
+(defun eff-binary (overlay)
   "Mode for hex dump for a sections OVERLAY."
   (let* ((section (overlay-get overlay 'section))
          (offset (overlay-get overlay 'offset))
          (size (overlay-get overlay 'size))
          (buffer-name (format "%s(%s)" (buffer-name) section))
-         (file-name (eff-mode-buffer-file-name))
+         (file-name (eff-buffer-file-name))
          (default-directory (file-name-directory file-name))
-         (command (format eff-mode-binary-command offset size (file-name-nondirectory file-name))))
+         (command (format eff-binary-command offset size (file-name-nondirectory file-name))))
     (with-current-buffer (pop-to-buffer buffer-name)
       (setq buffer-file-coding-system nil)
       (shell-command command (current-buffer))
@@ -260,34 +260,34 @@ Each element has the form (E_MACHINE . GDB).
       (hexl-mode)
       (read-only-mode))))
 
-(defconst eff-mode-syntax-table
+(defconst eff-syntax-table
   (let ((st (make-syntax-table)))
     (modify-syntax-entry ?_ "w" st)
     st))
 
-(defvar eff-mode-map (make-keymap))
-(suppress-keymap eff-mode-map)
+(defvar eff-map (make-keymap))
+(suppress-keymap eff-map)
 
-(defmacro eff-mode-create-key-binding (arg)
+(defmacro eff-create-key-binding (arg)
   "Create a mode key function for ARG state."
-  `(defun ,(intern (concat "eff-mode-" arg)) ()
+  `(defun ,(intern (concat "eff-" arg)) ()
      "Macro-generated key-binding function that changes buffer type"
      (interactive)
-     (setq eff-mode-buffer-type (intern ,arg))
+     (setq eff-buffer-type (intern ,arg))
      (eff-elf-revert-buffer)))
 
 (mapc
  (lambda (x)
    (let* ((key (cdr (assoc 'key x)))
           (state-name (car x)))
-     (eval (macroexpand-1 `(eff-mode-create-key-binding ,(symbol-name state-name))))
-     (define-key eff-mode-map key (intern (concat "eff-mode-" (symbol-name state-name))))))
- eff-mode-buffer-types)
+     (eval (macroexpand-1 `(eff-create-key-binding ,(symbol-name state-name))))
+     (define-key eff-map key (intern (concat "eff-" (symbol-name state-name))))))
+ eff-buffer-types)
 
 
 ;;;###autoload
 (define-derived-mode eff-mode special-mode "Elf"
-  :syntax-table eff-mode-syntax-table
+  :syntax-table eff-syntax-table
   (buffer-disable-undo)
   (eff-elf-revert-buffer))
 
@@ -296,6 +296,6 @@ Each element has the form (E_MACHINE . GDB).
 
 (add-to-list 'magic-mode-alist '("^!<arch>\012/       " . eff-mode))
 
-(provide 'eff-mode)
+(provide 'eff)
 
-;;; eff-mode.el ends here
+;;; eff.el ends here
